7f5053cdc957c3e54d1b184f51b5d547be57ab12
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-18 16:50:36 +0600

    Split Join

---
+++ split-join.js
@@ -0,0 +1,7 @@
+const lyrics = 'Dekha na dile bondhu. kotha koiyo naa';
+const parts = lyrics.split(' ');
+console.log(parts);
+const sentence = lyrics.split('.');
+console.log(sentence);
+const word = lyrics.split('');
+console.log(word);
