8de0b651465a5c78e9f665b627e9ff641b70cf40
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-19 00:08:36 +0600

    Max JS

---
+++ max.js
@@ -0,0 +1,23 @@
+/* const jim = 80;
+const dela = 85;
+if(jim > dela){
+    console.log('Jim Will get the cake');
+}
+else{
+    console.log('Dela wil get the cake');
+} */
+//---------------------------------------
+
+const jim = 84;
+const dela = 99;
+const chinku = 101;
+
+if(jim > dela && jim > chinku){
+    console.log('Jim will get the cake');
+}
+else if(dela > jim && dela > chinku){
+    console.log("Dela Will get the cake");
+}
+else{
+    console.log('Chinku will get the cake');
+}
